bba557a8f25dc86c671a74c7aed0891da44fd92d
bright1993 <75425626@qq.com>
2021-04-19 13:10:06 +0800

    add some paths for this project

---
+++ Analysis/data_paths.py
@@ -0,0 +1,18 @@
+import os
+
+# project paths
+project_code_path = r'XXX'
+count_tweet_month_path = os.path.join(project_code_path, 'count_tweets_month')
+count_tweet_quarter_path = os.path.join(project_code_path, 'count_tweets_quarter')
+count_daily_hour_path = os.path.join(project_code_path, 'count_tweets_daily_hour')
+figures_path = os.path.join(project_code_path, 'figures')
+
+# tweet datasets
+tweet_data = r'XXX'
+atlanta = os.path.join(tweet_data, 'Atlanta_Boston')
+boston = os.path.join(tweet_data, 'Atlanta_Boston')
+singapore = os.path.join(tweet_data, 'Singapore')
+hong_kong = os.path.join(tweet_data, 'hong_kong')
+
+# shapefile database
+shapefiles = r'XXX'
