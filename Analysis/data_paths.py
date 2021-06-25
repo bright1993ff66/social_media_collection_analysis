237400fe4888cffd46d3a5d04cc89a2d1a810f54
bright1993 <75425626@qq.com>
2021-06-25 16:01:50 +0800

    update the data paths

--- Analysis/data_paths.py
+++ Analysis/data_paths.py
@@ -7,12 +7,8 @@ count_tweet_quarter_path = os.path.join(project_code_path, 'count_tweets_quarter
 count_daily_hour_path = os.path.join(project_code_path, 'count_tweets_daily_hour')
 figures_path = os.path.join(project_code_path, 'figures')
 
-# tweet datasets
-tweet_data = r'XXX'
-atlanta = os.path.join(tweet_data, 'Atlanta_Boston')
-boston = os.path.join(tweet_data, 'Atlanta_Boston')
-singapore = os.path.join(tweet_data, 'Singapore')
-hong_kong = os.path.join(tweet_data, 'hong_kong')
+# raster save path
+raster_save_path = r'XXX'
 
-# shapefile database
-shapefiles = r'XXX'
+# NDVI path
+ndvi_path = r'XXX'
